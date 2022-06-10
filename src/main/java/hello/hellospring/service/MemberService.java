ce73fe34f01e35c8e366e08f4320031f417d73e6
yangbyoungseok <ybs991@naver.com>
2022-06-10 22:09:57 +0900

    스프링 AOP

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -6,11 +6,13 @@ import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
 
